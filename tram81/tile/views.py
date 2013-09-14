c6e77c62a352e213ee9ed751d4e7e609d224ed8a
Pierre Marchand <pierremarc07@gmail.com>
2013-09-14 16:37:20 +0200

    First multiply

--- tram81/tile/views.py
+++ tram81/tile/views.py
@@ -1,9 +1,12 @@
 # Create your views here.
 
 import os
+from collections import namedtuple
 
 from django.conf import settings
 from django.http import HttpResponse, Http404
+from django.contrib.gis.geos import Polygon
+
 
 import mapnik
 mapnik.logger.set_severity(mapnik.severity_type.Error)
@@ -13,6 +16,10 @@ from .tilenames import tileEdges
 import math
 from threading import Lock
 
+from api.models import GeoImage
+from PIL import Image
+from StringIO import StringIO
+
 def tile_to_longlat(x, y, z):
   n = 2.0 ** z
   lon_deg = x / n * 360.0 - 180.0
@@ -20,8 +27,111 @@ def tile_to_longlat(x, y, z):
   lat_deg = math.degrees(lat_rad)
   return (lat_deg, lon_deg)
 
-EPSG_3857_proj = mapnik.Projection('+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs')
-longlat_proj = mapnik.Projection('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+EPSG_3857_PROJ = mapnik.Projection('+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs')
+LONGLAT_PROJ = mapnik.Projection('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+
+IntBox = namedtuple('Box',('minx','miny','maxx','maxy'), verbose=True)
+
+def box_to_poly(box):
+    bl = (box.minx,box.miny)
+    tl = (box.minx,box.maxy)
+    tr = (box.maxx,box.maxy)
+    br = (box.maxx,box.miny)
+    return Polygon((bl,tl,tr,br,bl))
+
+def poly_to_box(poly):
+    x_pts = [t[0] for t in poly.envelope.coords[0]]
+    y_pts = [t[1] for t in poly.envelope.coords[0]]
+    minx = reduce(lambda a,b: min(a,b), x_pts)
+    maxx = reduce(lambda a,b: max(a,b), x_pts)
+    miny = reduce(lambda a,b: min(a,b), y_pts)
+    maxy = reduce(lambda a,b: max(a,b), y_pts)
+    return mapnik.Box2d(minx,miny,maxx,maxy)
+
+def round_box(box):
+    return IntBox(*map(int,map(round, box)))
+
+class ImageMixer(object):
+    def __init__(self):
+        pass
+    
+    
+    def op_multiply(self, base, top, base_box, top_box):
+        bpix = base.load()
+        #tpix = top.load()
+        
+        byy = range(base_box.maxy -1, base_box.miny -1, -1)
+        bxx = range(base_box.minx, base_box.maxx)
+        
+        sz = (base_box.maxx - base_box.minx, 
+              base_box.maxy - base_box.miny)
+        quad = ( top_box.minx, top_box.miny, # tl
+                top_box.minx, top_box.maxy,  # bl
+                top_box.maxx, top_box.maxy,  # br
+                top_box.maxx, top_box.miny)  # tr
+        tpix = top.transform(sz, Image.QUAD, quad, Image.BICUBIC).load()
+        
+        for y_idx in xrange(len(byy)):
+            by = byy[y_idx]
+            for x_idx in xrange(len(bxx)):
+                bx = bxx[x_idx]
+                vb = bpix[bx,by]
+                vt = tpix[x_idx, y_idx]
+                v =  tuple(map(lambda a,b: a*b/255, vb, vt))
+                bpix[bx,by] = v
+                
+    def _ts(self, a, min_, max_, T):
+        print '_TS %f %f %f %f'%(a, min_, max_, T)
+        r = a - min_
+        return r * float(T) / (max_ - min_)
+    
+        
+    def mix(self, tile, bbox):
+        wkt = box_to_poly(bbox.forward(LONGLAT_PROJ)).wkt
+        geo_images = GeoImage.objects.filter(geom__bboverlaps=wkt)
+        
+        
+        if geo_images:
+            src_image = Image.open(StringIO(tile.tostring('png')))
+            base = Image.new(src_image.mode, src_image.size, "white")
+        
+            for geo_image in geo_images:
+                box = poly_to_box(geo_image.geom)
+                ibox = box.intersect(bbox)
+                if (ibox.width <= 0.0) or (ibox.height <= 0.0):
+                    continue
+                
+                try:
+                    im = Image.open(geo_image.image.path)
+                except Exception:
+                    pass
+                
+                # project intersection on image
+                ox = self._ts(ibox.minx, box.minx, box.maxx, im.size[0])
+                oy = self._ts(ibox.maxy, box.maxy, box.miny, im.size[1])
+                dx = self._ts(ibox.maxx, box.minx, box.maxx, im.size[0])
+                dy = self._ts(ibox.miny, box.maxy, box.miny, im.size[1])
+                O = round_box(mapnik.Box2d(ox,oy,dx,dy))
+                
+                # project intersection on tile
+                b_ox = self._ts(ibox.minx, bbox.minx, bbox.maxx, src_image.size[0])
+                b_oy = self._ts(ibox.maxy, bbox.maxy, bbox.miny, src_image.size[1])
+                b_dx = self._ts(ibox.maxx, bbox.minx, bbox.maxx, src_image.size[0])
+                b_dy = self._ts(ibox.miny, bbox.maxy, bbox.miny, src_image.size[1])
+                B = round_box(mapnik.Box2d(b_ox, b_oy, b_dx, b_dy))
+                
+                self.op_multiply(base, im, B, O)
+                
+            AB = round_box(mapnik.Box2d(0,0,base.size[0],base.size[1]))
+            self.op_multiply(base, src_image, AB, AB)
+            
+            f = StringIO()
+            base.save(f, 'PNG')
+            return mapnik.Image.fromstring(f.getvalue())
+                
+        return tile
+                
+        
 
 class Map(object):
     
@@ -39,15 +149,15 @@ class Map(object):
         #try:
             #self.map.srs = settings.MAPNIK_MAP_SRS
         #except AttributeError:
-            #self.map.srs = EPSG_3857_proj.params()
+            #self.map.srs = EPSG_3857_PROJ.params()
            
         self.map.zoom_to_box(self.map.maximum_extent)
         
         self.proj = mapnik.Projection(self.map.srs)
-        self.transform = mapnik.ProjTransform(longlat_proj, self.proj)
+        self.transform = mapnik.ProjTransform(LONGLAT_PROJ, self.proj)
         
         #self.map.zoom_all()
-        #self.inverse_transform = mapnik.ProjTransform(self.proj, longlat_proj)
+        #self.inverse_transform = mapnik.ProjTransform(self.proj, LONGLAT_PROJ)
         #mb = self.map.envelope()
         #print 'Map extent: %s; latlon => %s'%(mb, self.inverse_transform.forward(mb))
         
@@ -94,7 +204,7 @@ class Map(object):
         finally:
             self.mutex.release()
 
-        return im
+        return ImageMixer().mix(im, bbox)
 
 
 class MapPool(object):
