f8433bafccc18e906a5ea8eb972d7ba6576fd313
Pierre Marchand <pierremarc07@gmail.com>
2013-09-17 07:57:45 +0200

    tile_server settings directive

--- tram81/tram81/views.py
+++ tram81/tram81/views.py
@@ -17,8 +17,14 @@
 """
 
 from django.views.generic import TemplateView
+from django.conf import settings
 
 class IndexView(TemplateView):
     template_name = "base.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['TILE_SERVER'] = settings.TILE_SERVER
+        return context
     
     
