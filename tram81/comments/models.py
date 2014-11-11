c325a80cbfbd112bdec28c5bbae29447b92124ce
Pierre Marchand <pierremarc07@gmail.com>
2014-11-11 23:01:06 +0100

    https://docs.djangoproject.com/en/dev/topics/auth/customizing/#django.contrib.auth.get_user_model

--- tram81/comments/models.py
+++ tram81/comments/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth import get_user_model
 from django.core.mail import mail_managers
-
-USER_MODEL = get_user_model()
+from django.conf import settings
 
 
 class Comment(models.Model):
-    commenter = models.ForeignKey(USER_MODEL)
+    commenter = models.ForeignKey(settings.AUTH_USER_MODEL)
     
     content = models.TextField(default=u'', blank=True)
     ts = models.DateTimeField(auto_now=True)
